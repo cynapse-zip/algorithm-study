3629dfbfc09b77c712e596e17437d81eb52fc490
Soyeong Park <cindy0585@naver.com>
2026-02-08 22:48:35 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 6일차 - 회전, Time: 61 ms, Memory: 49,920 KB -BaekjoonHub

--- SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
+++ SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
@@ -1,6 +1,12 @@
-# queue 없이 풀기 : 나머지 개념이랑 같음
-T = int(input().strip())
+T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
     arr = list(map(int, input().split()))
-    print(f"#{tc} {arr[M % N]}")
+
+    for _ in range(M):      # M번만큼 반복
+        # deQueue
+        first_num = arr.pop(0)
+        # enQueue
+        arr.append(first_num) 
+ 
+    print(f'#{tc} {arr[0]}')
